c4595716fd46e7499a32ccafd3b40309da6adb4a
Pascal Lombardet <thelomb@gmail.com>
2018-08-18 11:29:32 +0200

    url_for instead of hard coded url

--- application/routes.py
+++ application/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from application import app
 from application.forms import LoginForm
 
@@ -25,5 +25,5 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash(f'Login requested for {form.username.data}, {form.remember_me.data}')
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
