08e7163fb5cf885fd62f717ba2980f34769ea80e
Pascal Lombardet <thelomb@gmail.com>
2018-08-26 13:06:58 +0200

    update route to include page count

--- application/main/routes.py
+++ application/main/routes.py
@@ -47,12 +47,15 @@ def user(username):
     page = request.args.get('page', 1, type=int)
     posts = user.posts.order_by(Post.timestamp.desc()).paginate(
         page, current_app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('main.user', username=user.username, page=posts.next_num) \
+    next_url = url_for('main.user', username=user.username,
+                       page=posts.next_num) \
         if posts.has_next else None
-    prev_url = url_for('main.user', username=user.username, page=posts.prev_num) \
+    prev_url = url_for('main.user', username=user.username,
+                       page=posts.prev_num) \
         if posts.has_prev else None
     return render_template('user.html', user=user, posts=posts.items,
-                           next_url=next_url, prev_url=prev_url)
+                           next_url=next_url, prev_url=prev_url,
+                           pages=posts.pages)
 
 
 @bp.route('/edit_profile', methods=['GET', 'POST'])
